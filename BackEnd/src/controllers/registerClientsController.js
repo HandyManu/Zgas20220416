7ac8448d5dba37d6c964297af6e5e9331bd9f4de
Manuel <20220416@ricaldone.edu.sv>
2025-04-08 07:18:57 -0600

    solo me faltaba escribir un email , por eso no funcionaba pero ya esta

--- BackEnd/src/controllers/registerClientsController.js
+++ BackEnd/src/controllers/registerClientsController.js
@@ -51,8 +51,8 @@ registerClientsController.register=async (req,res)=>{
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: config.email_user,
-            pass: config.email_pass,
+            user: config.email.email_user,
+            pass: config.email.email_pass,
         },
     })
 
