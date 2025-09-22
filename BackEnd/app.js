d07a057bfc91bfd761efff93c16fe30f155d97cc
Manuel <20220416@ricaldone.edu.sv>
2025-09-22 14:14:53 -0600

    no se

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,25 +1,26 @@
 //importo todos los elemnetos de la ibreria express 
 
-import express from "express";
-import corse from "cors"; 
+import express from "express"
+import corse from "cors"
+import productRoutes from "../BackEnd/src/routes/products.js"
 import ClientsRoutes from "../BackEnd/src/routes/clients.js"
 import CostumersRoutes from "../BackEnd/src/routes/costumers.js"
 import branchRoutes from "../BackEnd/src/routes/branch.js"
-import ReviewsRouters from "../BackEnd/src/routes/review.js";
-import EvaluationRouters from "../BackEnd/src/routes/evaluations.js";
-import registerEmployeesRoutes from "../BackEnd/src/routes/registerEmployee.js";
-import cookieParser from "cookie-parser";
-import loginRoutes from "../BackEnd/src/routes/login.js";
-import logoutRoutes from "../BackEnd/src/routes/logout.js";
-import registerClients from "../BackEnd/src/routes/registerClients.js";
-import blogRoutes from "../BackEnd/src/routes/blog.js"; //importo la ruta de los blogs
-import faqsRoutes from "../BackEnd/src/routes/faqs.js"; //importo la ruta de los faqs
-import salesRoutes from "../BackEnd/src/routes/sales.js"; // Importo la ruta de ventas
-import { validateAuthToken } from "./src/middlewares/validateAuthToken.js";
-import swaggerUi from "swagger-ui-express";
-import fs from "fs";
-import path from "path";
-import limiter from "./src/middlewares/rateLimiter.js";
+import ReviewsRouters from "../BackEnd/src/routes/review.js"
+import EvaluationRouters from "../BackEnd/src/routes/evaluations.js"
+import registerEmployeesRoutes from "../BackEnd/src/routes/registerEmployee.js"
+import cookieParser from "cookie-parser"
+import loginRoutes from "../BackEnd/src/routes/login.js"
+import logoutRoutes from "../BackEnd/src/routes/logout.js"
+import registerClients from "../BackEnd/src/routes/registerClients.js"
+import blogRoutes from "../BackEnd/src/routes/blog.js"//importo la ruta de los blogs
+import faqsRoutes from "../BackEnd/src/routes/faqs.js" //importo la ruta de los faqs
+import salesRoutes from "../BackEnd/src/routes/sales.js" // Importo la ruta de ventas
+import { validateAuthToken } from "./src/middlewares/validateAuthToken.js"
+import swaggerUi from "swagger-ui-express"
+import fs from "fs"
+import path from "path"
+import limiter from "./src/middlewares/rateLimiter.js"
 
 //creo una contante d ela libreria que acabo de importar y la ejecuto
 
@@ -43,6 +44,7 @@ const swaggerDocumment = JSON.parse(
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumment));
 
 app.use(express.json());
+app.use("/api/products",productRoutes);
 app.use("/api/clients",ClientsRoutes);
 app.use("/api/costumers",CostumersRoutes);
 app.use("/api/branch",branchRoutes);
